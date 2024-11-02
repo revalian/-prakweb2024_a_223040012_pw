1047803a15129ae99d604cb6e9578ac2c170a30f
revalian <revalian@gmail.com>
2024-11-02 15:42:57 +0700

    (VIDEO 6) Membuat Aplikasi MVC dengan PHP #6 Assets

---
+++ mvc/App/Core/Constants.php
@@ -0,0 +1,4 @@
+<?php 
+
+
+define('BASEURL', 'http://localhost/prakweb2024_a_223040012_pw/mvc/public')?>
--- mvc/App/init.php
+++ mvc/App/init.php
@@ -4,4 +4,6 @@
 require_once 'Core/App.php';
 require_once 'Core/Controller.php';
 
+require_once 'core/Constants.php';
+
 ?>
